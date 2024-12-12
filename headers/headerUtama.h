1e9a24dee3cd73f6859bdea6afcfd1d4c84a9e74
yandik <yanandhinayaardika@gmail.com>
2024-12-12 21:29:40 +0700

    Undo, enter, arrow (#3)
    * yanardika
    
    * fix undo func, enter and another

--- headers/headerUtama.h
+++ headers/headerUtama.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
+#include <iostream>
 using namespace std;
 
 struct ElmHuruf;
@@ -19,11 +20,13 @@ struct ListHuruf
     adrHuruf last;
 };
 
-const int MAXSIZE = 4;
+const int MAXSIZE = 9999;
 typedef char infotypeS;
 typedef int index;
 struct Stack
 {
+    string cek[MAXSIZE];
+    adrHuruf cursor[MAXSIZE];
     infotypeS info[MAXSIZE];
     index top;
 };
@@ -44,15 +47,17 @@ struct Queue
     address head;
     address tail;
 };
+
+void tampilan();
 void printListSemua(ListHuruf L);
 void createList(ListHuruf &L);
 adrHuruf createElm(char x);
+void insertUtama(ListHuruf &L, adrHuruf P, Stack &stackUndo);
 void insertFirst(ListHuruf &L, adrHuruf P);
-void insertLast(ListHuruf &L, adrHuruf P);
-void insertAfter(ListHuruf &L, adrHuruf P, adrHuruf pred);
+void insertBefore(ListHuruf &L, adrHuruf P, adrHuruf pred);
 void deleteFirst(ListHuruf &L, adrHuruf P);
 void deleteLast(ListHuruf &L, adrHuruf P);
-void deleteAfter(ListHuruf &L, adrHuruf P, adrHuruf pred);
+void deleteBefore(ListHuruf &L, adrHuruf &P, adrHuruf pred);
 void printList(ListHuruf L);
 void printHuruf(ListHuruf L);
 adrHuruf cariCursor(ListHuruf L);
@@ -62,8 +67,8 @@ void cursorGeserKanan(ListHuruf &L);
 void createStack(Stack &S);
 bool isEmptyS(Stack S);
 bool isFullS(Stack S);
-void push(Stack &S, char P);
-void pop(Stack &stacks, char &P);
+void push(Stack &S, char P, string cek, adrHuruf cursor);
+void pop(Stack &S, char &P, string &cek, adrHuruf &cursor);
 infotypeS peek(Stack S);
 int sizeS(Stack S);
 void printStack(Stack S);
@@ -76,6 +81,9 @@ address front(Queue Q);
 infotypeQ sizeQ(Queue Q);
 
 void redoHuruf(ListHuruf &kalimat, Stack &stackUndo, Stack &stackRedo);
-void funcUndo(ListHuruf &L, Stack &stackUndo, Stack &stackRedo);
+void undoHuruf(ListHuruf &L, Stack &stackUndo, Stack &stackRedo);
+void handleEnter(ListHuruf &L);
+void handleBackspace(ListHuruf &L);
+adrHuruf cariCursor(ListHuruf L) ;
 
 #endif // HEADER_H_INCLUDED
